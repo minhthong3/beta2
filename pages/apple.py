acfa37e940ea46f310d6ac5860f56c00aeef5929
minhthong3 <63642154+minhthong3@users.noreply.github.com>
2024-05-31 23:14:51 +0700

    Update apple.py

--- pages/apple.py
+++ pages/apple.py
@@ -1,3 +1,8 @@
 # Tiêu đề của ứng dụng
 st.title("Apple")
-
+    match menu_item:
+        case 'apple':
+            st.page_link('pages/apple.py')
+    
+        case 'google':
+            st.page_link('pages/google.py')
