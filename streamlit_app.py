d1054bb7aad908a9d6d8a39fd72c4b1e8bea70ea
minhthong3 <63642154+minhthong3@users.noreply.github.com>
2024-05-31 23:12:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,5 +21,12 @@ with st.sidebar:
         ]),
     ], open_all=True)
 
+    match menu_item:
+    case 'apple':
+        st.page_link('pages/apple.py')
+
+    case 'google':
+        st.page_link('pages/google.py')
+
 
 
